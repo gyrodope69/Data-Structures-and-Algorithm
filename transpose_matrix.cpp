3ee15a39f9e0855595313ccb14679c6f0e3bade5
gyrodope69 <86244576+gyrodope69@users.noreply.github.com>
2022-05-30 18:22:47 +0530

    transpose_matrix.cpp

---
+++ transpose_matrix.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n][n];
+
+    // Taking iput for our matrix
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+        // cout<<"\n";
+    }
+
+    // Transposing the matrix
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            // int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            // arr[j][i] = temp;
+            
+
+    // Printig the transpose of matrix
+    cout << "Transpose of matrix : \n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
