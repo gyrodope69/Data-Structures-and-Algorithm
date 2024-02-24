7b7444a5073ffffd33f80f5acfe65c85a2a72daa
Rohit Mohan Roy <86244576+gyrodope69@users.noreply.github.com>
2024-02-25 03:46:50 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,3 +1,20 @@
+// templated SDN
+def handle_spoof(self, attacker_ip, attacker_mac, msg):
+    actions = []
+    in_port = msg.match['in_port']
+    datapath = msg.datapath
+    ofproto = datapath.ofproto
+    parser = datapath.ofproto_parser
+    inst = [parser.OFPInstructionActions(ofproto.OFPIT_CLEAR_ACTIONS, [])]
+    match = parser.OFPMatch(in_port=in_port, eth_src=attacker_mac, ipv4_src=attacker_ip)
+    mod = parser.OFPFlowMod(
+        datapath=datapath, match=match, idle_timeout=60, hard_timeout=60, priority=20, instructions=inst
+    )
+    datapath.send_msg(mod)
+    self.logger.info("\033[1;31m" + "Blocked traffic from IP %s and MAC %s on port %s" + "\033[0m", attacker_ip, attacker_mac, in_port)
+
+
+
 // code for bubble sort in array 
 
 
